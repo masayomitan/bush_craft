2e1a3cb7d3dce0e61282ad4aca13987d68caac50
kawasaki <yomitanmasa3523@gmail.com>
2023-08-13 19:40:55 +0900

    a

--- src/components/parts/about.tsx
+++ src/components/parts/about.tsx
@@ -6,8 +6,12 @@ const About = () => {
         <>
             <section className="">
                 <div className="container mx-auto mt-10 p-4">
-                    <h2 className="text-5xl font-bold">About us</h2>
-                    <div className="mt-4 text-lg leading-relaxed">
+                    <h2 className="text-5xl font-bold relative pb-2">
+                        About us
+                        <span className="absolute bottom-0 left-0 w-full h-2 bg-yellow-300 transform -skew-x-36"></span>
+                    </h2>
+                    <p className="text-1xl mb-2">テキストテキストテキスト</p>
+                    <div className="mt-4 text-1lg leading-relaxed">
                         テキスト<br />
                         テキストテキストテキストテキストテキストテキストテキストテキスト<br />
                         テキストテキストテキストテキストテキストテキストテキストテキスト<br />
--- src/components/parts/topExplain.tsx
+++ src/components/parts/topExplain.tsx
@@ -5,8 +5,12 @@ const TopExplain = () => {
     return (
         <>
             <div className="p-4 text-gray-800">
-                <h2 className="text-5xl font-bold">Bushcraft</h2>
-                <div className="mt-4 text-lg leading-relaxed">
+                <h2 className="text-5xl font-bold relative pb-2">
+                    Bushcraft
+                    <span className="absolute bottom-0 left-0 w-full h-2 bg-blue-300 transform -skew-x-36"></span>
+                </h2>
+                <p className="text-1xl mb-2">テキストテキストテキスト</p>
+                <div className="mt-4 text-1lg leading-relaxed">
                     テキスト<br />
                     テキストテキストテキストテキストテキストテキストテキストテキスト<br />
                     テキストテキストテキストテキストテキストテキストテキストテキスト<br />
@@ -18,13 +22,19 @@ const TopExplain = () => {
 
 
             <div className="p-4 text-gray-800">
-                <h2 className="text-5xl font-bold mb-4">体験</h2>
-                <p className="text-xl mb-2">テキストテキストテキスト</p>
-                <p className="text-xl mb-2">
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-                </p>
+                <h2 className="text-5xl font-bold relative pb-2">
+                    体験
+                    <span className="absolute bottom-0 left-0 w-full h-2 bg-green-300 transform -skew-x-36"></span>
+                </h2>
+                <p className="text-1xl mb-2">テキストテキストテキスト</p>
+                <div className="mt-4 text-1lg leading-relaxed">
+                    テキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                </div>
                 <p className="text-lg mb-2">テキストテキストテキストテキスト<Link href="https://note.com/your_username"className="text-blue-500 hover:underline">note</Link> に掲載しています。</p>
             </div>
 
@@ -32,19 +42,19 @@ const TopExplain = () => {
                 {/* 宿泊型スクールのカード */}
                 <div className="m-2.5 bg-white rounded-lg shadow-md p-4">
                     {/* 緑のラベル */}
-                    <div className="bg-teal-500 text-white rounded-full px-3 py-1 mb-4 text-sm font-semibold">宿泊型スクール</div>
+                    <div className="bg-teal-500 text-white rounded-full px-3 py-1 mb-4 text-sm font-semibold">宿泊型アクティブ</div>
                     
-                    <h2 className="text-xl font-bold mb-2">宿泊型スクール</h2>
-                    <p className="text-gray-700 text-base mb-4">宿泊型スクールは、実践的なブッシュクラフト体験を通じて、ひとりひとりが主体となりブッシュクラフトの総合的な技術・知識を学ぶプログラムとなります。個々の力を伸ばすことで、自ら生き抜く力を身につけ問題を解決する力を養うことを目標としています。</p>
+                    <h2 className="text-xl font-bold mb-2">1泊</h2>
+                    <p className="text-gray-700 text-base mb-4">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
                     
                     <div className="mb-2">
-                    <strong className="text-gray-800">●開催場所</strong> 関東近郊
+                        <strong className="text-gray-800">●開催場所</strong> テキストテキストテキストテキスト
                     </div>
                     <div className="mb-2">
-                    <strong className="text-gray-800">●移動手段</strong> 貸切バス、公共交通機関の利用及び現地集合・解散などスクールの形態により異なります。
+                        <strong className="text-gray-800">●移動手段</strong> テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
                     </div>
                     <div className="mb-2">
-                    <strong className="text-gray-800">●参加対象</strong> 小学3年生～中学生となります。
+                        <strong className="text-gray-800">●参加対象</strong> テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
                     </div>
                 </div>
             </div>
@@ -53,19 +63,19 @@ const TopExplain = () => {
                 {/* 日帰り型スクールのカード */}
                 <div className="m-2.5 bg-white rounded-lg shadow-md p-4">
                     {/* darkorangeのラベル */}
-                    <div className="bg-orange-700 text-white rounded-full px-3 py-1 mb-4 text-sm font-semibold">日帰り型スクール</div>
+                    <div className="bg-orange-700 text-white rounded-full px-3 py-1 mb-4 text-sm font-semibold">日帰り型アクティブ</div>
                     
-                    <h2 className="text-xl font-bold mb-2">日帰り型スクール</h2>
-                    <p className="text-gray-700 text-base mb-4">日帰り型スクールは、基本的なブッシュクラフトの技術・知識を学ぶプログラムとなります。その他、親子でご参加いただけるファミリーイベントも開催しています。</p>
+                    <h2 className="text-xl font-bold mb-2">日帰り</h2>
+                    <p className="text-gray-700 text-base mb-4">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
                     
                     <div className="mb-2">
-                    <strong className="text-gray-800">●開催場所</strong> 関東近郊
+                        <strong className="text-gray-800">●開催場所</strong> テキストテキストテキストテキスト
                     </div>
                     <div className="mb-2">
-                    <strong className="text-gray-800">●移動手段</strong> 現地集合・解散、または貸切バス、公共交通機関の利用などスクールの形態により異なります。
+                        <strong className="text-gray-800">●移動手段</strong> テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
                     </div>
                     <div className="mb-2">
-                    <strong className="text-gray-800">●参加対象</strong> スクール毎に異なります。
+                        <strong className="text-gray-800">●参加対象</strong> テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
                     </div>
                 </div>
             </div>
